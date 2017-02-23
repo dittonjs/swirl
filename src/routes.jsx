5d911609d286995a496972c75e98b2c5814ce875
ianjm24 <ianjm24@hotmail.com>
2017-02-22 20:38:32 -0700

    added profile import to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,7 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App                                        from './app';
 import Landing                                    from './components/landing';
 import Login                                      from './components/login';
-
+import Profile                                    from './components/profile/_profile';
 export default (
   <Router history={hashHistory}>
     <Route path="/" component={App}>
