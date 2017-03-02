e1a04896ef5e807db85bfc632cf36f015311e0b4
dittonjs <jditton.atomic@gmail.com>
2017-03-01 19:51:03 -0700

    update header

--- src/components/bathroomEdit/_bathroomEdit.jsx
+++ src/components/bathroomEdit/_bathroomEdit.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import _ from 'lodash';
 import ApplicationRoute from '../application_route';
-import pageHeader from '../pageHeader';
+import PageHeader from '../pageHeader';
 
 export default class BathroomEdit extends React.Component {
     render(){
         return(
-            <pageHeader></pageHeader>
+            <PageHeader pageName="Create Bathroom"/>
         );
     }
 }
--- src/components/pageHeader.jsx
+++ src/components/pageHeader.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import MaterialButton from './material_components/material_button';
 
-export default class pageHeader extends React.Component {
+export default class PageHeader extends React.Component {
     render(){
         return(
-            <div>
-                hey
-                <MaterialButton>
-                    Login
+            <div className="paper-2 header">
+                <MaterialButton type="flat">
+                    <i className="material-icons">menu</i>
                 </MaterialButton>
-                <MaterialButton>
-                    Leaderboard
-                </MaterialButton>
-                <MaterialButton>
-                    My Account
-                </MaterialButton>
+                <h3>{this.props.pageName}</h3>
             </div>
         );
     }
