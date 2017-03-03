fdfe656759ed81a8d3b1255ee0f8bb3970353b6d
ianjm24 <ianjm24@hotmail.com>
2017-03-03 10:30:31 -0700

    changed profile page

--- src/components/bathroomEdit/_bathroomEdit.jsx
+++ src/components/bathroomEdit/_bathroomEdit.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import _ from 'lodash';
 import ApplicationRoute from '../application_route';
 import PageHeader from '../pageHeader';
+import ContentArea from '../content_area';
+import FormElement from '../material_components/formElement';
+import FormStar from '../material_components/formStars';
 
 export default class BathroomEdit extends ApplicationRoute {
-    render(){
+    render() {
         return(
-            <PageHeader pageName="Create Bathroom"/>
+		<ContentArea>
+			<FormElement labelName="Bathroom Location (Engineering Building 1st Floor)" elementID="bathroomName" inputType="text"/>
+			<FormStar labelName="Bathroom Rating" elementID="bathroomRating"  />
+		</ContentArea>
         );
     }
 }
